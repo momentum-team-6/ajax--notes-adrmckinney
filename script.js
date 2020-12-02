6820241e52d201de50d05c66536b73b97d092c06
Daniel McKinney <adrmckinney@gmail.com>
2020-12-01 23:27:12 -0500

    working updateNote request. Trying to get updated dateStamp to display. It shows in the server but not on the page. Old date remains, can't get it to go away. Also need to finish the drag and drop. It only drags but won't drop.

--- script.js
+++ script.js
@@ -1,134 +1,12 @@
-// const { create } = require("json-server")
-
-// const momentMin = require("./moment.min")
-
-const notesApiUrl = 'http://localhost:3000/notes/'
-const form = document.querySelector('#note-writing-container')
-
-let loadedNotes = []
-let updatedNote = null
-
-function displayNotes(notes) {
-    const savedNoteList = document.querySelector('#saved-note-list')
-    savedNoteList.innerHTML = ""
-    console.log(savedNoteList)
-    for (let note of notes) {
-        console.log(note)
-        // create an element for the note
-        const noteElement = document.createElement('div')
-        noteElement.classList.add("note")
-        
-        // create title for note and add it to noteElement
-        const title = document.createElement('h3')
-        title.innerText = note.title
-        noteElement.appendChild(title)
-
-        // create body for note and add it to noteElement
-        const body = document.createElement('p')
-        body.innerText = note.body
-        noteElement.appendChild(body)
-        
-        // Add note element to saved-note-list
-        savedNoteList.appendChild(noteElement)
-
-        // Add dynamic delete button
-        const deleteButton = document.createElement('button')
-        deleteButton.innerText = 'Delete'
-        deleteButton.addEventListener('click', function(event) {
-            event.preventDefault()
-            deleteNote(note.id)
-        })
-        deleteButton.classList.add('delete-button')
-        noteElement.appendChild(deleteButton)
-
-        // create dynamic update note
-        const updateButton = document.createElement('button')
-        updateButton.innerText = 'Update'
-        updateButton.classList.add('update-button')
-        updateButton.addEventListener('click', function(event) {
-            event.preventDefault()
-            const submitButton = document.querySelector('#submit-button')
-            submitButton.innerText = 'Update Note'
-            updatedNote = note
-            document.querySelector('#note-title').value = note.title
-            document.querySelector('#note-body').value = note.body
-            document.querySelector('#note-date').value = note.created_at
-        })
-        noteElement.appendChild(updateButton)
-
-        // create a dynamic date stamp
-        const dateStamp = document.createElement('div')
-        dateStamp.id = '#note-date'
-        dateStamp.innerText = 'Created on' + " " + note.created_at
-        noteElement.appendChild(dateStamp)
-    }
+function dateStamp() {
+  let newDate = new Date()
+  let year = newDate.getFullYear()
+  let month = monthNumberToString(newDate.getMonth())
+  let day = newDate.getDate()
+  return month + " " + day + ', ' + year
 }
+// dateStamp()
 
-// API interactions
-
-function getAllNotes() {
-    fetch(notesApiUrl)
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(notes) {
-        loadedNotes = notes
-        displayNotes(loadedNotes)
-    })
-}
-
-function addNote(title, body, created_at) {
-    fetch(notesApiUrl, {
-        method: 'POST', 
-        headers: {"Content-Type": "application/json"}, 
-        body: JSON.stringify({"title": title, "body": body, 'created_at': moment().format("MMM Do YYYY")})
-    })
-    .then(r => r.json())
-    .then(function(note) {
-        console.log(note)
-        loadedNotes.push(note)
-        displayNotes(loadedNotes)
-    })
-}
-
-function deleteNote(id) {
-    fetch(`${notesApiUrl}${id}`, {
-        method: 'DELETE',
-        headers: {'Content-Type': 'application/json'},
-    })
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function() {
-        const noteToRemove = loadedNotes.find(function(note) {
-            return note.id === id
-        })
-        const noteIndexToRemove = loadedNotes.indexOf(noteToRemove)
-        loadedNotes.splice(noteIndexToRemove, 1)
-        displayNotes(loadedNotes)
-    })
-    
-}
-
-function updateNote(id, title, body) {
-    console.log(id, title, body)
-    fetch(`${notesApiUrl}${id}`, {
-        method: "PATCH",
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'title': title, 'body': body, 'created_at': moment().format()})
-    })
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(updatedNote) {
-        const noteToUpdate = loadedNotes.find(function(note) {
-            return note.id === updatedNote.id
-        })
-        const noteIndexToUpdate = loadedNotes.indexOf(noteToUpdate)
-        loadedNotes[noteIndexToUpdate] = updatedNote
-        displayNotes(loadedNotes)
-    })
-}
 function monthNumberToString(month){
     if (month === 0) {
         return "Jan"
@@ -157,35 +35,300 @@ function monthNumberToString(month){
     }
 }
 
-// function noteDate(month, day, year) {
-//     let d = new Date()
-//     return monthNumberToString(d.getMonth(month))
+
+const notesApiUrl = 'http://localhost:3000/notes/'
+const submitButton = document.querySelector('#submit-container')
+
+let tagNotes = {work: [], school: [], family: [], play: [], misc: []}
+let updatedNote = null
+
+
+function toggleNoteDisplayMode(note, isEditing) {
+    const viewNoteContainer = document.querySelector(`#view-note-${note.id}`)
+    const editNoteContainer = document.querySelector(`#edit-note-${note.id}`)
     
-// }
-
-// user events
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault()
-    const title = document.querySelector('#note-title').value
-    const body = document.querySelector('#note-body').value
-    // const created_at = document.querySelector('#note-date').value
-    console.log(title, body)
-    if (updatedNote) {
-        updateNote(updatedNote.id, title, body)
+    if (isEditing) {
+        viewNoteContainer.style.display = "none"
+        editNoteContainer.style.display = "flex"
     } else {
-        addNote(title, body)
+        viewNoteContainer.style.display = "flex"
+        editNoteContainer.style.display = "none"
+    }
+    
+    
+}
+
+function diplayViewNote(note, parent) {
+    // create view mode container
+    const viewContainer = document.createElement('div')
+    viewContainer.classList.add('view')
+    viewContainer.id = `view-note-${note.id}`
+    parent.appendChild(viewContainer)
+
+    // create noteBodyDate container
+    const noteBodyDateContainer = document.createElement('div')
+    noteBodyDateContainer.classList.add('note-body-date-container')
+    viewContainer.appendChild(noteBodyDateContainer)
+
+    // create a body element to go inside the noteBodyDateContainer
+    const body = document.createElement('div')
+    body.innerText = note.body
+    body.classList.add('note-body')
+    body.id = 'note-body'
+    noteBodyDateContainer.appendChild(body)
+
+    // create note date to go inside noteBodyDateContainer
+    const noteDate = document.createElement('div')
+    noteDate.id = 'note-date'
+    noteDate.innerText = 'Created on' + ' ' + note.created_at
+    noteBodyDateContainer.appendChild(noteDate)
+
+    // create an actions container to house delete and update buttons
+    const actions = document.createElement('div')
+    actions.classList.add('actions')
+    viewContainer.appendChild(actions)
+
+    // delete button
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('fas')
+    deleteButton.classList.add('fa-trash')
+    deleteButton.classList.add('action-buttons')
+    deleteButton.addEventListener('click', function(event) {
+        event.preventDefault
+        deleteNote(note.category, note.id)
+    })
+    actions.appendChild(deleteButton)
+
+    // update button
+    const updateButton = document.createElement('button')
+    updateButton.classList.add('fas')
+    updateButton.classList.add('fa-pen-alt')
+    updateButton.classList.add('action-buttons')
+    updateButton.addEventListener('click', function(event) {
+        event.preventDefault
+        toggleNoteDisplayMode(note, true)
+
+    })
+    actions.appendChild(updateButton)
+} 
+
+function displayEditNote(note, parent) {
+    // create edit mode container
+    const editContainer = document.createElement('div')
+    // hide the edit mode by default
+    editContainer.style.display = 'none'
+    editContainer.id = `edit-note-${note.id}`
+    editContainer.classList.add('edit')
+    parent.appendChild(editContainer)
+
+    // create noteBodyDate container
+    const noteBodyDateContainer = document.createElement('div')
+    noteBodyDateContainer.classList.add('note-body-date-container')
+    editContainer.appendChild(noteBodyDateContainer)
+
+    // create a body element to go inside the noteBodyDateContainer
+    const body = document.createElement('textarea')
+    body.value = note.body
+    body.classList.add('edit-note-body')
+    // body.classList.add('note-body')
+    body.id = `note-body-${note.id}`
+    noteBodyDateContainer.appendChild(body)
+
+    // create note date to go inside noteBodyDateContainer
+    const noteDate = document.createElement('div')
+    noteDate.id = 'note-date'
+    noteDate.innerText = 'Created on' + ' ' + note.created_at
+    noteBodyDateContainer.appendChild(noteDate)
+
+    // create an actions container to house delete and update buttons
+    const actions = document.createElement('div')
+    actions.classList.add('actions')
+    editContainer.appendChild(actions)
+
+    // cancel button
+    const cancelButton = document.createElement('button')
+    cancelButton.classList.add('far')
+    cancelButton.classList.add('fa-window-close')
+    cancelButton.classList.add('action-buttons')
+    cancelButton.addEventListener('click', function(event) {
+        event.preventDefault
+        toggleNoteDisplayMode(note, false)
+    })
+    actions.appendChild(cancelButton)
+
+    // save button
+    const saveButton = document.createElement('button')
+    saveButton.classList.add('far')
+    saveButton.classList.add('fa-check-square')
+    saveButton.classList.add('action-buttons')
+    saveButton.addEventListener('click', function(event) {
+        event.preventDefault
+        
+        toggleNoteDisplayMode(note, false)
+        const body = document.querySelector(`#note-body-${note.id}`).value
+        updateNote(note.id, note.category, body, note.updated_at)
+        
+        // null created_at date
+        noteDate.value = ''
+
+        // create update note date to go inside noteBodyDateContainer
+        const updateNoteDate = document.createElement('div')
+        updateNoteDate.id = 'note-date'
+        updateNoteDate.innerText = 'Updated on' + ' ' + note.updated_at
+        noteBodyDateContainer.appendChild(updateNoteDate)
+    })
+    actions.appendChild(saveButton)
+}
+
+function displayNotesImproved(tagNotes) {
+    const categories = Object.keys(tagNotes)
+
+    
+    // Provide category selection options for user
+    const categorySelect = document.querySelector("#category-input")
+    categorySelect.innerHTML = ''
+    
+
+    const savedNotes = document.querySelector('#saved-note-container')
+    savedNotes.innerHTML = ''
+    for (let categoryName of categories) {
+        console.log(categoryName)
+        
+        const categoryOption = document.createElement('option')
+        categoryOption.innerText = categoryName
+        categorySelect.appendChild(categoryOption)
+
+        let notesForCategory = tagNotes[categoryName]
+        if (notesForCategory.length === 0) {
+            continue
+        }
+
+        // create container for notes
+        const categoryContainer = document.createElement('div')
+        categoryContainer.classList.add("categoryContainer")
+        savedNotes.appendChild(categoryContainer)
+        
+        // create element for category
+        const category = document.createElement('h2')
+        category.innerText = categoryName
+        category.classList.add('category')
+        category.id = 'note-category'
+        categoryContainer.appendChild(category)
+
+        // create loop for the arrays (i.e., note bodies) of the categories
+        const categoryContainers = []
+
+        for(let note of notesForCategory) {
+            
+            // create contentContainer to house view and edit modes. View and edit modes will each contain noteBodyDate and actions containers
+            const contentContainer = document.createElement('div')
+            contentContainer.classList.add('content-container')
+            categoryContainer.appendChild(contentContainer)
+            categoryContainers.push(categoryContainer)
+            diplayViewNote(note, contentContainer)
+            displayEditNote(note, contentContainer)
+            
+        }
+
+        dragula(categoryContainers).on('over', function(element) {
+            element.classList.add('hover')
+            console.log(element)
+        })
+        
     }
 
-    document.querySelector('#note-title').value = ""
-    document.querySelector('#note-body').value = ""
-    // document.querySelector('#note-date').value = ""
-    document.querySelector('#submit-button').innerHTML = 'Create Note'
-    updatedNote = null
+}
 
-    }
-)
+// API interactions
+function getAllNotes() {
+    fetch(notesApiUrl)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(notes) {
+        console.log(notes)
+        for (let note of notes) {
+            if (note.category === 'school')
+            tagNotes.school.push(note)
+            
+            else if (note.category === 'work') {
+                tagNotes.work.push(note)
+            }
+            else if (note.category === 'family') {
+                tagNotes.family.push(note)
+            }
+            else if (note.category === 'play') {
+                tagNotes.play.push(note)
+            }
+            else if (note.category === 'misc') {
+                tagNotes.misc.push(note)
+            }
+        }
+            
+        displayNotesImproved(tagNotes)
+    })
+}
 
-// gets all notes to display for the first page load. This should always be last
+function addNote(category, body, created_at) {
+    fetch(notesApiUrl, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({category: category, body: body, created_at: dateStamp()})
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(note) {
+        console.log(note)
+        tagNotes[category].push(note)
+        displayNotesImproved(tagNotes)
+    })
+}
+
+function deleteNote(category, id) {
+    fetch(`${notesApiUrl}${id}`, {
+        method: 'DELETE',
+    })
+    .then(function() {
+        const noteToRemove = tagNotes[category].find(function(note) {
+            return note.id === id
+        })
+        const noteIndexToRemove = tagNotes[category].indexOf(noteToRemove)
+        tagNotes[category].splice(noteIndexToRemove, 1)
+        displayNotesImproved(tagNotes)
+    })
+}
+
+function updateNote(id, category, body, updated_at) {
+    fetch(`${notesApiUrl}${id}`, {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({body: body, updated_at: dateStamp()})
+    })
+    .then(response => response.json())
+    .then(function(updatedNote) {
+        // notes for category eg., family. This is the array of notes for a given category.
+        const notesForCategory = tagNotes[category]
+        const noteToUpdate = notesForCategory.find(note => note.id === updatedNote.id)
+        const noteIndexToUpdate = notesForCategory.indexOf(noteToUpdate)
+        notesForCategory[noteIndexToUpdate] = updatedNote
+        tagNotes[category] = notesForCategory
+        displayNotesImproved(tagNotes)
+        console.log(tagNotes)
+    })
+}
+
+// const noteToUpdate = notesForCategory.find(function(note) {
+//     return note.id === updatedNote.id
+// })
+
+submitButton.addEventListener('submit', function(event) {
+    event.preventDefault()
+    const category = document.querySelector('#category-input').value
+    const body = document.querySelector('#note').value
+    console.log(category, body)
+    addNote(category, body)
+    document.querySelector('#note').value = ''
+})
+
 getAllNotes()
-
